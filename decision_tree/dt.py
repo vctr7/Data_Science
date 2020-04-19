d4988c9851dc40b0720195af852c2997bc2d5132
PARK JEE HO <n2201121@gmail.com>
2020-04-19 17:47:38 +0900

    project

--- decision_tree/dt.py
+++ decision_tree/dt.py
@@ -2,7 +2,6 @@ import sys
 import math
 import numpy as np
 
-test = {}
 
 def split_info(column, target_list):
     summation = 0
@@ -19,24 +18,24 @@ def split_info(column, target_list):
     return summation
 
 
-def getColNum(input_n):
-    with open(input_n, 'r', encoding='utf-8') as t:
-        for line in t:
-            k = len(line.split())
+def getColNum(file):
+    with open(file, 'r', encoding='utf-8') as t:
+        for line in file:
+            length = len(line.split())
             break
-    return k
+    return length
 
 
 def preprocess(file, k):
     f = open(file, 'r', encoding='utf-8')
     lines = f.readlines()
-    category_result = []
 
+    category_result = []
     for i in range(k):
         category = []
         for num, x in enumerate(lines):
-
             category.append(x.split()[i])
+
         category_result.append(category)
 
     return category_result[:-1], category_result[-1]
@@ -66,11 +65,11 @@ def entropy(a, b):
 
 
 def calculateAttrInfo(column, k):
-    set_ = set(column)
+    unique_elem = set(column)
     a = 0
     b = 0
     for elem in column:
-        if elem == list(set_)[0]:
+        if elem == list(unique_elem)[0]:
             a += 1
         else:
             b += 1
@@ -78,19 +77,19 @@ def calculateAttrInfo(column, k):
     return ((a + b) / k) * entropy(a, b)
 
 
-def getAttrInfo(column, target_list):
-    summation = 0
-    column_len = len(target_list)
-    reduced_column = list(set(column))
+def getAttrInfo(col, target_list):
+    info_sum = 0
+    col_len = len(target_list)
+    unique_col = list(set(col))
 
-    for elem in reduced_column:
+    for elem in unique_col:
         temp = []
-        for num, item in enumerate(column):
+        for num, item in enumerate(col):
             if elem == item:
                 temp.append(target_list[num])
-        summation += calculateAttrInfo(temp, column_len)
+        info_sum += calculateAttrInfo(temp, col_len)
 
-    return summation
+    return info_sum
 
 
 def gain(info_x, info_y):
@@ -122,50 +121,44 @@ def makeSubtree(attr_list, target_list, elem, max_idx):
     for n, item in enumerate(category_result):
         item.insert(0, attr_list[n][0])
 
+    # Delete the column of highest gain.
     del(category_result[max_idx])
 
     return category_result, n_target_list
 
 
-def DTProcess(attr_list, target_list, ntree, height):
+def leaf(target_list):
+    max_num = 0
+    ans = 0
+    for elem in set(target_list):
+        count = target_list.count(elem)
+        if max_num < count:
+            max_num = count
+            ans = elem
 
-    # max height. avoid over-fitting
+    return ans
+
+
+def DTProcess(attr_list, target_list, height):
+
+    # Set max height as 3 to avoid over-fitting problem
     if height > 3:
-        return set(target_list)
+        return leaf(target_list)
 
-    # print the height of node and its content
-    if height >= 0:
-        # print(height, target_list[1:])
-        height += 1
+    height += 1
 
-    # attr_list 비어있을 경우 return
+    # If attribute list is empty, return
     if len(attr_list) < 1:
-        max_num = 0
-        ans = 0
-        for i in set(target_list):
-            count = target_list.count(i)
-            if max_num < count:
-                max_num = count
-                ans = i
+        return leaf(target_list)
 
-        return ans
-
-    # 완벽히 분류되었을 시 return
+    # If target list is already classified, return
     elif len(set(target_list[1:])) == 1:
-        max_num = 0
-        ans = 0
-        for i in set(target_list):
-            count = target_list.count(i)
-            if max_num < count:
-                max_num = count
-                ans = i
-
-        return ans
+        return leaf(target_list)
 
     # 우선 들어온 데이터에 대해 info 계산해서 best 정해야함.
     info_d = calculateInfoD(target_list[1:])
 
-    # attr 별로 gain 계산해야 함..
+    # Calculate the Gain on each attribute
     gain_list = []
     for column in attr_list:
         column_info = getAttrInfo(column[1:], target_list[1:])
@@ -177,7 +170,7 @@ def DTProcess(attr_list, target_list, ntree, height):
     #     s_info = split_info(column[1:], target_list[1:])
     #     split_list.append(gain(info_d, column_info)/s_info)
 
-    # attr_list 중, max gain 을 가진 attr 구한다
+    # Find the highest gain among the attributes.
     max_gain = 0
     max_idx = 0
     for num, i in enumerate(gain_list):
@@ -191,54 +184,82 @@ def DTProcess(attr_list, target_list, ntree, height):
     #         max_gain = i
     #         max_idx = num
 
-    # parent node 기준에 맞는 자식 nodes 생성
+    # Make tree recursively based on the Dictionary structure
     best = attr_list[max_idx][0]
     tree = {best: {}}
 
+    # Find the subtrees of current node.
     for num, elem in enumerate(set(attr_list[max_idx][1:])):
         new_attr_list, new_target = makeSubtree(attr_list, target_list, elem, max_idx)
-        ntree.append(attr_list[max_idx][0])
-        subtree = DTProcess(new_attr_list, new_target, ntree, height)
+        subtree = DTProcess(new_attr_list, new_target, height)
         tree[best][elem] = subtree
 
     return tree
 
 
+def predict(trx, attr, tree):
+
+    if not isinstance(tree, dict):
+        return tree
+
+    for key, subtree in tree.items():
+        for n, item in enumerate(attr):
+            if key == item:
+                return predict(trx, attr, subtree)
+
+            elif key == trx[n]:
+                return predict(trx, attr, subtree)
+
+    return tree
 
 
 def classifier(file, tree):
-    col = getColNum(file)
     f = open(file, 'r', encoding='utf-8')
     lines = f.readlines()
     category_result = []
 
-    for i in range(col):
+    for i in range(getColNum(file)):
         category = []
-        for num, x in enumerate(lines):
+        for x in lines:
             category.append(x.split()[i])
         category_result.append(category)
 
     category_result = np.array(category_result).T
-    print(tree)
+
     t_attr = category_result[0]
     t_attr_list = category_result[1:]
+    result = []
+    for trx in t_attr_list:
+        result.append(predict(trx, t_attr, tree))
 
-    for n, line in enumerate(t_attr_list):
-        trx = []
-        for num, item in enumerate(line):
-            trx.append([t_attr[num], item])
-        print(n+1, trx)
+    return result
+
+
+def save(train, test, result, output):
+
+    first_line = open(train, 'r', encoding='utf-8').readline()
+    output_file = open(output, 'w', encoding='utf-8')
+    output_file.writelines(first_line)
+
+    with open(test, 'r', encoding='utf-8') as f:
+        for n, line in enumerate(f):
+            if n == 0:
+                continue
+
+            new_line = line.strip() + '\t' + result[n-1] + '\n'
+            output_file.write(new_line)
+
+        f.close()
 
 
 def main():
     input_train = sys.argv[1]
     input_test = sys.argv[2]
     output_file = sys.argv[3]
-    tree_info = []
-    col = getColNum(input_train)
-    attr_list, target_list = preprocess(input_train, col)
-    tree = DTProcess(attr_list, target_list, tree_info, height=0)
-    classifier(input_test, tree)
+
+    attr_list, target_list = preprocess(input_train, getColNum(input_train))
+    result = classifier(input_test, DTProcess(attr_list, target_list, height=0))
+    save(input_train, input_test, result, output_file)
 
 
 if __name__ == "__main__":
